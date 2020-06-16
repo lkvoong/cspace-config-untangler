6c27da71fe05e4fa6397836650189cc7e60ee6f8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-06-16 14:40:03 -0400

    Test for bug associated with adding rectype option to profile

--- spec/cspace_config_untangler/profile_spec.rb
+++ spec/cspace_config_untangler/profile_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe CCU::Profile do
   before do
-    stub_const('CCU::CONFIGDIR', 'spec/fixtures/files/5_2')
+    stub_const('CCU::CONFIGDIR', 'spec/fixtures/files/6_0')
   end
 
   let(:core_profile) { CCU::Profile.new('core') }
@@ -11,7 +11,7 @@ RSpec.describe CCU::Profile do
   let(:core_rectypes) { ['acquisition', 'citation', 'collectionobject', 'concept', 'conditioncheck', 'conservation', 'exhibition', 'group', 'intake', 'loanin', 'loanout', 'location', 'media', 'movement', 'objectexit', 'organization', 'person', 'place', 'uoc', 'valuation', 'work'] }
   let(:core_authorities) { %w[citation/local citation/worldcat concept/activity concept/associated concept/ethculture concept/material concept/nomenclature location/local location/offsite organization/local organization/ulan person/local person/ulan place/local place/tgn work/cona work/local] }
   let(:core_option_lists) { ['dimensions', 'measurementUnits', 'searchResultPagePageSizes', 'searchPanelPageSizes', 'booleans', 'yesNoValues', 'dateQualifiers', 'departments', 'loanPurposes', 'accountStatuses', 'acquisitionMethods', 'citationTermStatuses', 'ageUnits', 'collections', 'contentObjectTypes', 'forms', 'inscriptionTypes', 'measuredParts', 'measurementMethods', 'nameCurrencies', 'nameLevels', 'nameSystems', 'nameTypes', 'numberTypes', 'objectComponentNames', 'objectStatuses', 'ownershipAccessLevels', 'ownershipCategories', 'ownershipExchangeMethods', 'phases', 'positions', 'recordStatuses', 'scripts', 'sexes', 'technicalAttributes', 'technicalAttributeMeasurements', 'technicalAttributeMeasurementUnits', 'titleTypes', 'objectParentTypes', 'objectChildTypes', 'conceptTermStatuses', 'conceptTermTypes', 'conceptHistoricalStatuses', 'objectAuditCategories', 'completenessLevels', 'conditions', 'conservationTreatmentPriorities', 'hazards', 'conditionCheckMethods', 'conditionCheckReasons', 'salvagePriorityCodes', 'emailTypes', 'telephoneNumberTypes', 'faxNumberTypes', 'webAddressTypes', 'addressTypes', 'addressCountries', 'exhibitionConsTreatmentStatuses', 'exhibitionMountStatuses', 'entryReasons', 'locationTermTypes', 'locationTermStatuses', 'mediaTypes', 'locationFitnesses', 'moveReasons', 'moveMethods', 'invActions', 'invFreqs', 'exitReasons', 'exitMethods', 'orgTermTypes', 'orgTermStatuses', 'personTermStatuses', 'personTermTypes', 'salutations', 'personTitles', 'genders', 'placeTermTypes', 'placeTermStatuses', 'placeHistoricalStatuses', 'placeTypes', 'coordinateSystems', 'spatialRefSystems', 'localityUnits', 'geodeticDatums', 'geoRefProtocols', 'geoRefVerificationStatuses', 'reportMimeTypes', 'valueTypes', 'vocabTermStatuses', 'workTermStatuses'].sort }
-  let(:core_vocabs) { ["acousticalproperties", "additionalprocesses", "additionalproperties", "addresstype", "agentinfotype", "agequalifier", "castingprocesses", "citationtermflag", "citationtermtype", "collectionmethod", "concepttermflag", "concepttype", "conditioncheckmethod", "conditioncheckreason", "conditionfitness", "conservationstatus", "currency", "datecertainty", "dateera", "datequalifier", "deaccessionapprovalgroup", "deaccessionapprovalstatus", "deformingprocesses", "disposalmethod", "dtstest", "dtstest1", "dtstest2", "durabilityproperties", "ecologicalcertifications", "electricalproperties", "energyunits", "entrymethod", "examinationphase", "exhibitionpersonrole", "exhibitionreferencetype", "exhibitionstatus", "exhibitiontype", "hygrothermalproperties", "hygrothermalpropertyunits", "inventorystatus", "joiningprocesses", "languages", "lifecyclecomponents", "loanoutstatus", "locationtermflag", "locationtype", "machiningprocesses", "materialform", "materialformtype", "materialproductionrole", "materialresource", "materialtermflag", "materialtype", "materialuse", "mechanicalproperties", "mechanicalpropertyunits", "moldingprocesses", "opticalproperties", "organizationtype", "orgtermflag", "otherpartyrole", "persontermflag", "persontermtype", "placetermflag", "publishto", "rapidprototypingprocesses", "recycledcontentqualifiers", "relationtypetype", "resourceidtype", "sensorialproperties", "smartmaterialproperties", "surfacingprocesses", "taxontermflag", "taxontype", "treatmentpurpose", "uocmethods", "uocusertypes", "workcreatortype", "workpublishertype", "worktermflag", "worktype"].sort }
+  let(:core_vocabs) { %w[acousticalproperties additionalprocesses additionalproperties addresstype agentinfotype agequalifier castingprocesses citationtermflag citationtermtype collectionmethod concepttermflag concepttype conditioncheckmethod conditioncheckreason conditionfitness conservationstatus contactrole contactstatus currency datecertainty dateera datequalifier deaccessionapprovalgroup deaccessionapprovalstatus deformingprocesses disposalmethod dtstest dtstest1 dtstest2 durabilityproperties ecologicalcertifications electricalproperties energyunits entrymethod examinationphase exhibitionpersonrole exhibitionreferencetype exhibitionstatus exhibitiontype hygrothermalproperties hygrothermalpropertyunits inventorystatus joiningprocesses languages lifecyclecomponents loanoutstatus locationtermflag locationtype machiningprocesses materialform materialformtype materialproductionrole materialresource materialtermflag materialtype materialuse mechanicalproperties mechanicalpropertyunits moldingprocesses opticalproperties organizationtype orgtermflag otherpartyrole persontermflag persontermtype placetermflag publishto rapidprototypingprocesses recycledcontentqualifiers relationtypetype resourceidtype sensorialproperties smartmaterialproperties surfacingprocesses taxontermflag taxontype treatmentpurpose uocauthorizationstatuses uoccollectiontypes uocmaterialtypes uocmethods uocprojectid uocstaffroles uocsubcollections uocuserroles uocusertypes workcreatortype workpublishertype worktermflag worktype].sort }
   
   describe '.new' do
     it 'creates CCU::Profile object' do
@@ -71,12 +71,20 @@ RSpec.describe CCU::Profile do
 
 
   describe '.authorities' do
+    context 'when all rectypes requested' do
     it 'returns array' do
       expect(core_profile.authorities).to be_instance_of(Array)
     end
 
     it 'returns expected authorities' do
       expect(core_profile.authorities.sort).to eq(core_authorities.sort)
+    end
+    end
+    context 'when only collectionobject requested' do
+      it 'returns expected authorities' do
+        p = CCU::Profile.new('core', rectypes: ['collectionobject'])
+        expect(p.authorities.sort).to eq(core_authorities.sort)
+      end
     end
   end
 
