d75540f48efcea6f5a3079ce6803a6f23bf2658c
kspurgin <kspurgin@users.noreply.github.com>
2021-02-25 14:25:55 -0500

    change base github path for mappers and refresh mappers.json manifest (#58)
    * change base github path for mappers and refresh mappers.json manifest file
    
    * bump version

--- lib/cspace_config_untangler.rb
+++ lib/cspace_config_untangler.rb
@@ -28,7 +28,7 @@ module CspaceConfigUntangler
   File.delete('log.log') if File::exist?('log.log')
   CCU.const_set('LOG', Logger.new('log.log'))
   CCU.const_set('MAPPER_DIR', "#{CCU::DATADIR}/mappers")
-  CCU.const_set('MAPPER_URI_BASE', 'https://raw.githubusercontent.com/collectionspace/cspace-config-untangler/master/data/mappers')
+  CCU.const_set('MAPPER_URI_BASE', 'https://raw.githubusercontent.com/collectionspace/cspace-config-untangler/main/data/mappers')
 
 
   autoload :VERSION, 'cspace_config_untangler/version'
