22c523880cbab34308f1466492cfae4a02c9b133
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-03-16 20:01:16 -0400

    Template for media rectype should include mediaFileURI column

--- lib/cspace_config_untangler/special_rectype.rb
+++ lib/cspace_config_untangler/special_rectype.rb
@@ -4,7 +4,7 @@ module CspaceConfigUntangler
   # methods to make special/manually created relationship rectypes act as much as
   #  real rectypes as they need to
   module SpecialRectype
-    def batch_mappings
+    def batch_mappings(context = :mapper)
       mappings
     end
   end
